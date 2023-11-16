f6625c1d417a4e113b1c60ed348ecf7e3a9ec999
Jeff <jeff@Jeffs-MacBook-Pro.local>
2023-11-16 21:52:30 +0100

    feat: initial commit

---
+++ src/main/java/com/springboot/EnableCorsJavaMvcConfigSpringMVC/EnableCorsJavaMvcConfigSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.EnableCorsJavaMvcConfigSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnableCorsJavaMvcConfigSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnableCorsJavaMvcConfigSpringMvcApplication.class, args);
+	}
+
+}
